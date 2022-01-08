430e84bf18e9c50fd420e42977aa6dc53c51571d
Den Rolya <denrolya@gmail.com>
2022-01-08 18:17:04 +0200

    feat(redux router): integrate redux hook with react router v6 and make use of it

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
+import { createRouterMiddleware } from '@lagunovsky/redux-react-router';
 
+import history from 'src/services/history';
 import reducer from 'src/store/reducer';
 
-const middlewares = [thunkMiddleware];
+const routerMiddleware = createRouterMiddleware(history);
+const middlewares = [thunkMiddleware, routerMiddleware];
 const composeEnhancers = composeWithDevTools({
   trace: true,
   traceLimit: 25,
