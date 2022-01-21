c2b591a9be9d20382c9ee38d66285998b39306dd
Den Rolya <denrolya@gmail.com>
2022-01-21 18:04:11 +0200

    refactor(exchange rates): wrap fetching into try/catch block

--- src/store/actions/exchangeRates.js
+++ src/store/actions/exchangeRates.js
@@ -1,6 +1,8 @@
-import axios from 'src/services/http';
 import { createActions } from 'reduxsauce';
+
 import Routing from 'src/services/routing';
+import axios from 'src/services/http';
+import { notify } from 'src/store/actions/global';
 
 export const { Types, Creators } = createActions(
   {
@@ -14,6 +16,11 @@ export const { Types, Creators } = createActions(
 export const fetch = () => async (dispatch) => {
   dispatch(Creators.fetchRequest());
 
-  const { data } = await axios.get(Routing.generate('api_v1_currency_rates'));
-  dispatch(Creators.fetchSuccess(data));
+  try {
+    const { data } = await axios.get(Routing.generate('api_v1_currency_rates'));
+    dispatch(Creators.fetchSuccess(data));
+  } catch (e) {
+    notify('error', '[Error]: Fetch Exchange Rates');
+    dispatch(Creators.fetchFailure(e));
+  }
 };
